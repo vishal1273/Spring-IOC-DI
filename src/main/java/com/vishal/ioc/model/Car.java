c60645bd39339b6834e3b8e8d56b78e8c2e2632f
Vishal Thakur <vishalthakur107@gmail.com>
2026-03-08 12:23:17 +0530

    demo: illustrate Spring annotation-based dependency injection using @Autowired and @Qualifier

--- src/main/java/com/vishal/ioc/model/Car.java
+++ src/main/java/com/vishal/ioc/model/Car.java
@@ -1,7 +1,13 @@
 package com.vishal.ioc.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Car {
     private String carName;
+
+    @Autowired
+    @Qualifier("engine2")
     private Engine engine;
 
     public void setCarName(String carName) {
